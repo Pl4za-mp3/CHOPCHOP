55b4d544308360a29196edcb2bee7f8fbdb73f3a
Pl4za-mp3 <j3sus.pla@gmail.com>
2025-12-11 13:20:09 -0400

    Update text.py

--- text.py
+++ text.py
@@ -8,5 +8,11 @@ win_size = 500, 385
 first_win_text = "Bienvenido a CHOP-CHOP. Presione el botón que ve abajo para continuar"
 first_win_button = "🍟CONTINUAR🍔"
 
+titulo_2 = "ChopChop"
 second_win_text = "Bienvenido, Por Favor escoja algo del menú!"
 menu = "parrilla mixta = 20$\n Pinchos de carne = 5$ c/u\n cachopo de lomito con papas = 10$\n nuggets con papas fritas = 8$\n solomillo = 15$\n Lipton = 3$\n jugo natural = 2,5$\n refresco = 2$"
+precios = {"parrilla mixta" : 20, "pinchos de carne" : 5, "cachopo de lomito con papas" : 10, "nuggets con papas fritas" : 8, "solomillo" : 15, "lipton" : 3, "jugo natural" : 2.5, "refresco" : 2}
+comida = 
+
+if comida in precios: 
+  print("Ha escogido", precio[comida])
